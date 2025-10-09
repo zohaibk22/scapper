fbec07e03254060dd2f0fdcf92bd3bc6e3712e82
Zohaib Khan <zohaibk1111@gmail.com>
2025-10-09 02:08:09 -0400

    added view

--- scapper/views.py
+++ scapper/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
 # Create your views here.
+def index(request):
+    page = requests.get('https://www.google.com')
+    soup = BeautifulSoup(page.content, 'html.parser')
+    link_address = []
+
+    for link in soup.find_all('a'):
+        link_address.append(link.get('href'))
+
+    return render(request=request, template_name='scapper/index.html', context={'links': link_address})
